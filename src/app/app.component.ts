7cd0117cd5236fb35171e59910ae1d6db10f8dcd
jawadEddassouli <eddassouli.jawad@iisgalvanimi.edu.it>
2022-03-10 09:04:47 +0000

    372 - Verifica02 - Angular Data Binding (Calcolatrice)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+
+  public num1:number = 0;
+  public num2:number = 0;
+  public risultato:number = 0;
+
+  div(){
+    this.risultato = this.num1 / this.num2
+  }
+  per(){
+    this.risultato = this.num1 * this.num2
+  }
+  sum(){
+    this.risultato = this.num1 + this.num2
+  }
+  sot(){
+    this.risultato = this.num1 - this.num2
+  }
 }
